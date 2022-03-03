78a08dc6d4b042e72dec3afc1a7e93aff85dc8e3
ozgencdev <ozgencdev@gmail.com>
2022-03-03 11:52:43 +0300

    final

--- members/DB.js
+++ members/DB.js
@@ -4,11 +4,13 @@ class DB {
   constructor() {
     this.tables = new Map();
   }
+
   createTable(tableName, columnName) {
     let columns;
     columns = createColumns(columnName, new Table());
     this.tables.set(tableName, columns);
   }
+
   getTable(tableName) {
     return this.tables.has(tableName) ? this.tables.get(tableName) : null;
   }
--- members/Table.js
+++ members/Table.js
@@ -3,29 +3,33 @@ class Table extends Map {
   constructor() {
     super();
   }
+
   insertRecords(record) {
     isValidType(record, "number");
     this.forEach((value, columnName) => {
       addData(columnName, record, this);
     });
   }
+
   getAllRecords() {
     let columns;
     columns = [];
     this.forEach((value, columnName) => {
       columns.push(value);
     });
+
     return zip(...columns);
   }
+
   filterRecords(value) {
     const recordValue = Object.values(value);
     const allRecord = this.getAllRecords();
-
     const filtered = allRecord.filter((record, index) => {
       return recordValue.every((value) => {
         return record.includes(value);
       });
     });
+
     return filtered;
   }
 }
